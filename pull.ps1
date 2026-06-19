9d5e70f0ad5a4146bae0aadd7e20013afc2e9d51
Hanue-coder <serenadefy@naver.com>
2026-06-19 17:34:58 +0900

    자동 업데이트 2026-06-19 17:34

---
+++ pull.ps1
@@ -0,0 +1,2 @@
+﻿cd C:\Users\USER\Documents\Korean_Public_Nonferrous_Metal_Price
+git pull
---
+++ sync.ps1
@@ -0,0 +1,4 @@
+﻿cd C:\Users\USER\Documents\Korean_Public_Nonferrous_Metal_Price
+git add .
+git commit -m "자동 업데이트 $(Get-Date -Format 'yyyy-MM-dd HH:mm')"
+git push
